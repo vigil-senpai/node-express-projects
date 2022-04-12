12b87f760b0acf26d27300533b575734ee1e6971
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-04-12 17:42:58 +0700

    env and mongoose setup

--- task-manager/start/app.js
+++ task-manager/start/app.js
@@ -1,20 +1,30 @@
 const express = require('express'); 
 const app = express(); 
 const tasks = require('./routes/tasks'); 
+const connection = require('./database/connect');
+require('dotenv').config(); 
+// require('dotenv').config({path: "./.env"}); 
+// require('dotenv/config')
 
 const port = 8000; 
+
+const startServer = async () => {
+    try {
+        await connection.connectDatabase(process.env.MONGO_URI); 
+        app.listen(port, () => {
+            console.log(`[+] App is listening on port ${port}`); 
+        }); 
+    }
+    catch (error) {
+        console.log(error); 
+    }
+}
 
 // MIDDLEWARE
 app.use(express.json()); 
 
 // ROUTES
-app.get('/hello', (req, res) => {
-    res.send('GG');
-}); 
 
 app.use('/api/v1/tasks', tasks); 
 
-// LISTENER
-app.listen(port, () => {
-    console.log(`[+] App is listening on port ${port}`); 
-}); 
+startServer()
